ed8304e3bcb086b3fbd10ea190cc58a3af1fdf2d
yaopiaohai <yaopiaohai@corp.netease.com>
2015-08-30 14:24:58 +0800

    misc:add v1

--- src/main/java/edu/xautjzd/dao/impl/UserDao.java
+++ src/main/java/edu/xautjzd/dao/impl/UserDao.java
@@ -5,6 +5,6 @@ import edu.xautjzd.dao.IUserDao;
 public class UserDao implements IUserDao {
 
 	public void add() {
-		System.out.println("------------from UserDao.add()");  
+		System.out.println("------------from UserDao.add() -- v1");  
 	}
 }
